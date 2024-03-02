040fd424d042fe28094b6333e44d5c7fd6ef1968
lovekesh-18 <lovekeshkumar3535@gmail.com>
2024-03-02 21:21:04 +0530

    python file

---
+++ Variables.py
@@ -0,0 +1,50 @@
+# Integer Variable
+
+x = 10
+print(x)
+
+# Float variable
+
+fv = 20.3
+print(fv)
+f = float(4)
+print(f)
+
+# gs are defined either with a single quote or a double quotes.
+str = "Lovekesh"
+print(str)
+
+s = 'Lovekesh'
+print(s)
+
+my_str = "Don't worry"
+print(my_str)
+
+# operators
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+name = "lovekesh"
+helloname = hello + ' ' + name
+print(helloname)
+
+a,b = 3,4
+print(a,b)
+
+# print(one + two + hello)   Error (int and str are not added)
+
+mystring = "lovekesh"
+myfloat = 10.0
+myint = 20
+
+if mystring == "lovekesh":
+    print("Name = %s" %mystring)
+if isinstance(myfloat , float) and myfloat == 10.0:
+    print("Float : %.2f" %myfloat)
+if isinstance(myint ,int) and  myint == 20:
+    print("Integer : %d" %myint)
+ 
+
